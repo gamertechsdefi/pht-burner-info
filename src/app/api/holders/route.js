ce2917234f6b56dc33f0728bf85ac7a3acc47177
Gamer <gamertechs.defi@gmail.com>
2025-02-12 20:43:10 +0100

    bug fixes and improv

--- src/app/api/holders/route.js
+++ src/app/api/holders/route.js
@@ -17,7 +17,8 @@ export async function GET(req) {
             WAR: "0x57bfe2af99aeb7a3de3bc0c42c22353742bfd20d",
             YUKAN: "0xd086B849a71867731D74D6bB5Df4f640de900171",
             BTCDRAGON: "0x1ee8a2f28586e542af677eb15fd00430f98d8fd8",
-            NENE: "0x551877C1A3378c3A4b697bE7f5f7111E88Ab4Af3"
+            NENE: "0x551877C1A3378c3A4b697bE7f5f7111E88Ab4Af3",
+            OCICAT: "0x37Fe635D1e25B2F7276C1B9dBBcc7b087f80C050"
         };
 
         const contractAddress = tokenData[tokenName.toUpperCase()];
